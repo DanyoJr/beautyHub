e6c72930fa064918c77f5a8cf708f13955a9e2a4
Daniel Yoneshige Junior <yoneshige@Daniels-MacBook-Pro.local>
2026-03-08 19:52:17 -0300

    clean enterprise index.post.ts

--- server/api/enterprise/index.post.ts
+++ server/api/enterprise/index.post.ts
@@ -1,34 +1,3 @@
 import Enterprise from "~~/server/models/Enterprise";
 
-export default defineEventHandler(async (event) => {
-  const cleanZipCode = zipcode.replace(/\D/g, "");
-
-  try {
-    const viaCepResponse: ViaCep = await $fetch(
-      `https://viacep.com.br/ws/${cleanZipCode}/json/`,
-    );
-
-    if (viaCepResponse.erro) {
-      throw createError({
-        statusCode: 400,
-        statusMessage: "CEP não encontrado na base de dados.",
-      });
-    }
-  } catch (error: any) {
-    if (error.statusCode) throw error;
-    throw createError({
-      statusCode: 500,
-      statusMessage: "Erro ao validar o CEP",
-    });
-  }
-
-  const address = {
-    zipcode,
-    state,
-    city,
-    neighborhood,
-    street,
-    number,
-    complement: complement ? complement : "",
-  };
-});
+export default defineEventHandler(async (event) => {});
